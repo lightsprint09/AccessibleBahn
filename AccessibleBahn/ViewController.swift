2c134ad06f0cee2ea08e520ef41f465d73e19f16
Lukas Schmidt <lukas.schmi@gmail.com>
2015-12-29 19:12:57 +0100

    Initial Commit

---
+++ AccessibleBahn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AccessibleBahn
+//
+//  Created by Lukas Schmidt on 29.12.15.
+//  Copyright © 2015 Lukas Schmidt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
